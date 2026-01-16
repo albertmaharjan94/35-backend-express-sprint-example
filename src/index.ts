fc422f2b1b0ddfbb89ae48fdb9ddc2aebe5cf653
albert <albert.maharjan94@gmail.com>
2026-01-16 10:36:18 +0545

    sprint 2

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,17 @@ import bodyParser from 'body-parser';
 import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
 import authRoutes from "./routes/auth.route";
+import cors from 'cors';
+
 
 const app: Application = express();
+
+const corsOptions = {
+    origin:[ 'http://localhost:3000', 'http://localhost:3003', 'http://localhost:3005' ],
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
