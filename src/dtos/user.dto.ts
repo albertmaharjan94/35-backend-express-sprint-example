3c0c5b9fad940616d8ecfb10f624515e7da7c659
albert <albert.maharjan94@gmail.com>
2026-01-27 13:58:19 +0545

    sprint 3

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -27,3 +27,6 @@ export const LoginUserDTO = z.object({
     password: z.string().min(6)
 });
 export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
+
+export const UpdateUserDTO = UserSchema.partial(); // all attributes optional
+export type UpdateUserDTO = z.infer<typeof UpdateUserDTO>;
---
+++ src/routes/admin/user.route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { authorizedMiddleware, adminMiddleware } from "../../middlewares/authorized.middleware";
+import { AdminUserController } from "../../controllers/admin/user.controller";
+
+let adminUserController = new AdminUserController();
+
+const router = Router();
+
+router.use(authorizedMiddleware); // apply all with middleware
+router.use(adminMiddleware); // apply all with middleware
+
+router.post("/", adminUserController.createUser);
+router.get("/", adminUserController.getAllUsers);
+router.put("/:id", adminUserController.updateUser);
+router.delete("/:id", adminUserController.deleteUser);
+router.get("/:id", adminUserController.getUserById);   
+
+export default router;
