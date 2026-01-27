39c999d4a85d4fcfc83ab8a3759c064ecea8dadc
albert <albert.maharjan94@gmail.com>
2026-01-27 15:43:43 +0545

    who am i

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
-
+import { authorizedMiddleware } from "../middlewares/authorized.middleware";
+import { uploads } from "../middlewares/upload.middleware";
 let authController = new AuthController();
 const router = Router();
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
 // add remaning routes like login, logout, etc.
+
+
+router.get("/whoami", authorizedMiddleware, authController.getProfile);
+
+router.put(
+    "/update-profile",
+    authorizedMiddleware,
+    uploads.single("image"), // "image" - field name from frontend/client
+    authController.updateProfile
+)
 
 export default router;
