e936fa1d729466642e6d4efc0deaf2330de3fbd3
albert <albert.maharjan94@gmail.com>
2026-02-05 09:42:21 +0545

    sprint-4

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,3 +1,4 @@
+import { QueryFilter } from "mongoose";
 import { UserModel, IUser } from "../models/user.model";
 export interface IUserRepository {
     getUserByEmail(email: string): Promise<IUser | null>;
@@ -6,7 +7,9 @@ export interface IUserRepository {
     // 5 common database queries for entity
     createUser(userData: Partial<IUser>): Promise<IUser>;
     getUserById(id: string): Promise<IUser | null>;
-    getAllUsers(): Promise<IUser[]>;
+    getAllUsers(
+        page: number, size: number, search?: string
+    ): Promise<{users: IUser[], total: number}>;
     updateUser(id: string, updateData: Partial<IUser>): Promise<IUser | null>;
     deleteUser(id: string): Promise<boolean>;
 }
@@ -30,9 +33,25 @@ export class UserRepository implements IUserRepository {
         const user = await UserModel.findById(id);
         return user;
     }
-    async getAllUsers(): Promise<IUser[]> {
-        const users = await UserModel.find();
-        return users;
+    async getAllUsers(
+        page: number, size: number, search?: string
+    ): Promise<{users: IUser[], total: number}> {
+        const filter: QueryFilter<IUser> = {};
+        if (search) {
+            filter.$or = [
+                { username: { $regex: search, $options: 'i' } },
+                { email: { $regex: search, $options: 'i' } },
+                { firstName: { $regex: search, $options: 'i' } },
+                { lastName: { $regex: search, $options: 'i' } },
+            ];
+        }
+        const [users, total] = await Promise.all([
+            UserModel.find(filter)
+                .skip((page - 1) * size)
+                .limit(size),
+            UserModel.countDocuments(filter)
+        ]);
+        return { users, total };
     }
     async updateUser(id: string, updateData: Partial<IUser>): Promise<IUser | null> {
         // UserModel.updateOne({ _id: id }, { $set: updateData });
